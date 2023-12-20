497602306b68391002c2d7d037850872b60717b9
daniel.hedegaard23 <daniel201002@live.dk>
2023-12-20 12:25:48 +0100

    lightlogprice model - no work tho

---
+++ Models/LightLogPrice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class LightLogPrice
+    {
+        public int Id { get; set; }
+        public int LightLogId { get; set; }
+        public Nullable<double> Price { get; set; }
+
+        //public int ProductId { get; set; }
+        //public DateTime DateSent { get; set; }
+        //public int Hours { get; set; }
+        //public int Minutes { get; set; }
+        //public int Seconds { get; set; }
+        //public Nullable<double> Price { get; set; }
+    }
+}
